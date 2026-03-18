c6477b0441255efeb3a66d41849af4658feaf79b
Hopulele Robert <roberthopulele35@gmail.com>
2026-03-18 13:08:00 +0200

    Lab4

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+return (
+  <div>
+    <h1>Dashboard</h1>
+    <h2>Test de pozitionare </h2>
+    <p>Hopulele Robert </p>
+    <ul>mere</ul>
+    <ul>pere</ul>
+    <p>Primul site jsx</p>
+  </div>
+);
+
+}
+
+export default App;
