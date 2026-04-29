222cd20550c7deb78b0783181bd78716a25abc64
Hopulele Robert <roberthopulele35@gmail.com>
2026-04-29 12:53:11 +0300

    Lab7-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
 import Card from './Card';
 import QuickNote from './QuickNote';
 import TodoList from './TodoList';
 import ContactForm from './ContactForm';
 import ProjectList from './ProjectList';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+ <Navbar>
+  <Routes>
+   <Route path="/" element={<Home />} />
+ <Route path="/projects" element={<Projects />} />
+ <Route path="/contact" element={<Contact />} />
+  </Routes>
+ </Navbar>
+ <Routes>
+ <Route path="/" element={<Home />} />
+ <Route path="/projects" element={<Projects />} />
+ <Route path="/contact" element={<Contact />} />
+ </Routes>
+ </BrowserRouter>
+      
       <Card/>
       <h1>Dashboard</h1>
       <h2>Test de pozitionare </h2>
