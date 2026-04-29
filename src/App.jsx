2b1eaeebaa77a457608d3973f62686c5512479ee
Hopulele Robert <roberthopulele35@gmail.com>
2026-04-29 13:42:05 +0300

    Lab7-5

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Navbar from './Navbar';
 import NotFound from './pages/NotFound';
+import About from './pages/About';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+    function About(){
+        return(
+            <div>
+                <h2>Sunt la facultate anul 2</h2>
+                <h3>Am facut o pagina despre mine</h3>
+            </div>
+        );
+    }
+    export default About;
