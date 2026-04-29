83e79e101b2f25e5e49134c378e12a1bb64fb8d5
Hopulele Robert <roberthopulele35@gmail.com>
2026-04-29 13:23:00 +0300

    Lab7-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,30 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Card from './Card';
 import QuickNote from './QuickNote';
 import TodoList from './TodoList';
-import ContactForm from './ContactForm';
+// import ContactForm from './ContactForm';
 import ProjectList from './ProjectList';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 import Navbar from './Navbar';
+import NotFound from './pages/NotFound';
 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
- <Navbar>
-  <Routes>
-   <Route path="/" element={<Home />} />
- <Route path="/projects" element={<Projects />} />
- <Route path="/contact" element={<Contact />} />
-  </Routes>
- </Navbar>
- <Routes>
- <Route path="/" element={<Home />} />
- <Route path="/projects" element={<Projects />} />
- <Route path="/contact" element={<Contact />} />
- </Routes>
- </BrowserRouter>
-      
-      <Card/>
-      <h1>Dashboard</h1>
-      <h2>Test de pozitionare </h2>
-      <p>Hopulele Robert </p>
-      <ul>mere</ul>
-      <ul>pere</ul>
-      <p>Primul site jsx</p>
+        <Navbar />
 
-      <ProjectList />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      <QuickNote />
+      </BrowserRouter>
 
-      <TodoList />
-
-      <ContactForm
-      Hello
-      />
     </div>
   );
 }
