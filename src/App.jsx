7df84ee4c2c28adb3df57013ba53406f6f7c387c
Robi3h4r <roberthopulele35@gmail.com>
2026-05-02 13:24:30 +0300

    Lab7_complet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import Card from './Card';
-import QuickNote from './QuickNote';
-import TodoList from './TodoList';
-// import ContactForm from './ContactForm';
-import ProjectList from './ProjectList';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
@@ -11,13 +6,11 @@ import Navbar from './Navbar';
 import NotFound from './pages/NotFound';
 import About from './pages/About';
 
-
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
@@ -25,13 +18,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-
       </BrowserRouter>
-
     </div>
   );
 }
 
 export default App;
-
-//hello 
