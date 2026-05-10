32f95661eb4656ed6a40f4b21ace2d3139c00e8f
Hopulele Robert <roberthopulele35@gmail.com>
2026-05-10 13:19:01 +0300

    Lab10-1

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
+
 const Project = require('./models/Project'); // Pas 1: Importăm modelul
 
 // Conectarea la baza de date
